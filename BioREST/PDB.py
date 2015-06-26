266e4ee1b6e3e70c2f0cd9501976a05025115f0b
Arnaud KOPP <kopp.arnaud@gmail.com>
2015-06-26 14:54:22 +0200

    Work on pdb service

--- BioREST/PDB.py
+++ BioREST/PDB.py
@@ -12,7 +12,7 @@ __email__ = "kopp.arnaud@gmail.com"
 __status__ = "Production"
 
 import logging
-from BioREST.Service import REST
+from BioREST.Service import REST, check_range, check_param_in_list
 
 log = logging.getLogger(__name__)
 
@@ -28,35 +28,80 @@ class PDB(REST):
         res = self.easyXML(res)
         return res
 
-    def describe_pdb_entity(self):
-        raise NotImplementedError
+    def status_pdb(self, id):
+        url = 'idStatus?structureId='
+        url += str(id)
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def describe_chemical_comp(self):
-        raise NotImplementedError
+    def describe_pdb_entity(self, id):
+        url = 'describeMol?structureId='
+        url += str(id)
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def get_ligand(self):
-        raise NotImplementedError
+    def describe_chemical_comp(self, id):
+        url = 'describeHet?chemicalID='
+        url += str(id)
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def release_status(self):
-        raise NotImplementedError
+    def get_current_PDB_ids(self):
+        url = 'getCurrent'
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def get_released_pdb(self):
-        raise NotImplementedError
+    def get_obsolete_PDB_ids(self):
+        url = 'getObsolete'
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def get_unreased_pdb(self):
-        raise NotImplementedError
+    def get_unreleased(self, id=None):
+        url = 'getUnreleased'
+        if id is not None:
+            url += '?structureId='+str(id)
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def get_pre_released_sequence(self):
-        raise NotImplementedError
+    def get_ligand(self, id):
+        url = 'ligandInfo?structureId='
+        url += str(id)
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def get_pdb_chains_anno(self):
-        raise NotImplementedError
+    def get_gene_ontology(self, id):
+        url = 'goTerms?structureId='
+        url += str(id)
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def get_uniprot_mapping(self):
-        raise NotImplementedError
+    def get_pfam_annot(self, id=None, tab_frmt=False):
+        url = 'hmmer'
+        if id is not None:
+            url += '?structureId='+str(id)
+        if tab_frmt:
+            if id is None:
+                url += '?file=hmmer_pdb_all.txt'
+        res = self.http_get(query=url, frmt='xml')
+        res = self.easyXML(res)
+        return res
 
-    def get_pfam_mapping(self):
-        raise NotImplementedError
-
-    def get_onto_terms(self):
-        raise NotImplementedError
+    def smiles_query(self, query, search_type='exact', similarity=None):
+        __valid_search_type = ['exact', 'substructure', 'superstructure', 'similarity']
+        check_param_in_list(search_type, __valid_search_type)
+        params = {'smiles': query, 'seach_type' : search_type}
+        if similarity is not None:
+            check_range(similarity, 0, 1)
+            params['similarity'] = similarity
+        url = 'smilesQuery'
+        res = self.http_get(query=url, frmt='xml', params=params)
+        res = self.easyXML(res)
+        return res
