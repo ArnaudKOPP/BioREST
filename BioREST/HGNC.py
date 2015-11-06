4e1eae78be5628c2706329740f25a86d2f75e50b
KOPP Arnaud <kopp.arnaud@gmail.com>
2015-11-06 13:38:34 +0100

    Add doc

--- BioREST/HGNC.py
+++ BioREST/HGNC.py
@@ -21,7 +21,8 @@ log = logging.getLogger(__name__)
 
 class HGNC(REST):
     """
-    Interface to the `HGNC <http://www.genenames.org>`_ service
+    Wrapper to the genenames web service
+    See details at http://www.genenames.org/help/rest-web-service-help
     """
     def __init__(self):
         url = "http://rest.genenames.org"
@@ -46,21 +47,55 @@ class HGNC(REST):
         """
         if storedfield not in self._storedFields:
             raise RestServiceError('StoredFields not valid')
-        query = 'fetch/'+str(storedfield)+'/'+str(term)
+        query = 'fetch/{0}/{1}'.format(storedfield, term)
         header = self.get_headers(content='json')
         res = self.http_get(query=query, headers=header)
         return res
 
     def search(self, searchablefields, term):
         """
-        Search data
+        Search a searchable field (database) for a pattern
+
+        The search request is more powerful than fetch for querying the
+        database, but search will only returns the fields hgnc_id, symbol and
+        score. This is because this tool is mainly intended to query the server
+        to find possible entries of interest or to check data (such as your own
+        symbols) rather than to fetch information about the genes. If you want
+        to retrieve all the data for a set of genes from the search result, the
+        user could use the hgnc_id returned by search to then fire off a fetch
+        request by hgnc_id.
+
         :param searchablefields:
         :param term:
         :return: :raise RestServiceError:
+        ::
+
+            # Search all searchable fields for the tern BRAF
+            h.search('BRAF')
+
+            # Return all records that have symbols that start with ZNF
+            h.search('symbol', 'ZNF*')
+
+            # Return all records that have symbols that start with ZNF
+            # followed by one and only one character (e.g. ZNF3)
+            # Nov 2015 does not work neither here nor in within in the
+            # official documentation
+            h.search('symbol', 'ZNF?')
+
+            # search for symbols starting with ZNF that have been approved
+            # by HGNC
+            h.search('symbol', 'ZNF*+AND+status:Approved')
+
+            # return ZNF3 and ZNF12
+            h.search('symbol', 'ZNF3+OR+ZNF12')
+
+            # Return all records that have symbols that start with ZNF which
+            # are not approved (ie entry withdrawn)
+            h.search('symbol', 'ZNF*+NOT+status:Approved')
         """
         if searchablefields not in self._searchableFields:
             raise RestServiceError('SearchableFields not valid')
-        query = 'seach/'+str(searchablefields)+'/'+str(term)
+        query = 'seach/{0}/{1}'.format(storedfield, term)
         header = self.get_headers(content='json')
         res = self.http_get(query=query, headers=header)
         return res
